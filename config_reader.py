e717127e480986e910244ccf0dde11e9f55ed75c
carlostcba <33902391+carlostcba@users.noreply.github.com>
2025-05-05 20:31:40 -0300

    Update config_reader.py

--- config_reader.py
+++ config_reader.py
@@ -1,15 +1,15 @@
-
 import pyodbc
 
 def get_config():
     conn = pyodbc.connect("DSN=LPR_MSSQL")
     cursor = conn.cursor()
-    cursor.execute("SELECT TOP 1 cam_ip, cam_user, cam_password, snapshot_path FROM DahuaConfig ORDER BY id")
+    cursor.execute("SELECT TOP 1 cam_ip, cam_user, cam_password, snapshot_path, images_folder FROM DahuaConfig ORDER BY id")
     row = cursor.fetchone()
     conn.close()
     return {
         "cam_ip": row.cam_ip,
         "cam_user": row.cam_user,
         "cam_password": row.cam_password,
-        "snapshot_path": row.snapshot_path
+        "snapshot_path": row.snapshot_path,
+        "images_folder": row.images_folder
     }
