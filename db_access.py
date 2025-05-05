915734eadfad1d00ce810387712859c348bd7e18
carlostcba <33902391+carlostcba@users.noreply.github.com>
2025-05-05 20:31:00 -0300

    Update db_access.py

--- db_access.py
+++ db_access.py
@@ -1,5 +1,5 @@
-
 import pyodbc
+from datetime import datetime
 
 def get_connection():
     return pyodbc.connect("DSN=LPR_MSSQL")
@@ -12,12 +12,12 @@ def is_plate_authorized(plate):
     conn.close()
     return row is not None and row.Habilitado == 1
 
-def log_event(plate, timestamp, image_url, status):
+def log_event(plate, timestamp, image_url, status, local_image_path=None):
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
-        INSERT INTO LPR_Logs (PlateNumber, EventTime, ImageURL, Status)
-        VALUES (?, ?, ?, ?)
-    """, plate, timestamp, image_url, status)
+        INSERT INTO LPR_Logs (PlateNumber, EventTime, ImageURL, Status, LocalImagePath)
+        VALUES (?, ?, ?, ?, ?)
+    """, plate, timestamp, image_url, status, local_image_path)
     conn.commit()
     conn.close()
